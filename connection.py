bd7bb86b174d5ff96a51d5745e2cd8ebdf7c649c
lrodrin <lrodrin@gmail.com>
2017-12-13 15:47:59 +0100

    update connection.py

--- connection.py
+++ connection.py
@@ -6,7 +6,13 @@ Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 from ncclient import manager
 
 n = manager.connect(host='10.1.7.81', port=830, username='root',
-                    password='netlabN.', device_params={'name': 'csr'})
+                    password='netlabN.', device_params={'name': 'csr'})  # device handler csr -> Cisco CSR
 
-schema = n.get_schema('toaster')
-print(schema)
+# for c in n.server_capabilities:
+#     print(c)
+
+yang_model = "toaster"
+schema = n.get_schema(yang_model)
+print(schema.data)
+
+n.close_session()
