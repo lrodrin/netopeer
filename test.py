ddf23441fbf7e856000129845ca4f91d70ca0ecd
lrodrin <lrodrin@gmail.com>
2017-12-15 13:27:52 +0100

    update project

--- test.py
+++ test.py
@@ -3,19 +3,34 @@ This module implements ...
 
 Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 """
-from ncclient import manager
+from ncclient import manager as m
 
-n = manager.connect(host='10.1.7.81', port=830, username='root',
+host = "10.1.7.81"
+n = m.connect(host=host, port=830, username='root',
                     password='netlabN.', device_params={'name': 'csr'})  # device handler csr -> Cisco CSR
-
-# for c in n.server_capabilities:
-#     print(c)
-
-yang_model = "toaster"
-schema = n.get_schema(yang_model)
-print(schema.data)
 
 for capability in n.server_capabilities:
     print(capability)
+
+# yang_model = "toaster"
+# schema = n.get_schema(yang_model)
+# print(schema.data)
+
+# c = n.get_config(source='running').data_xml
+# with open("%s.xml" % host, 'w') as file:
+#     file.write(c)
+
+rpc = """
+    <get-config>
+    <source>
+        <running/>
+    </source>
+    <filter xmlns="urn:opendaylight:params:xml:ns:yang:hello">
+        <hello/>
+    </filter>
+    </get-config>"""
+
+data = n.rpc(rpc)
+print(data)
 
 n.close_session()
