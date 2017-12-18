f3b7f2b30b580c26d9134770107ff7d791574059
lrodrin <lrodrin@gmail.com>
2017-12-18 11:30:10 +0100

    update project

--- test.py
+++ test.py
@@ -1,36 +1,49 @@
 """
-This module implements ...
+This module implements some code about the class ncclient, a NETCONF Python client
 
 Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 """
-from ncclient import manager as m
+from ncclient import manager
 
-host = "10.1.7.81"
-n = m.connect(host=host, port=830, username='root',
-                    password='netlabN.', device_params={'name': 'csr'})  # device handler csr -> Cisco CSR
 
-for capability in n.server_capabilities:
-    print(capability)
+def connect(host):
+    m = manager.connect(host=host, port=830, username='root', password='netlabN.', device_params={'name': 'csr'})  #
+    # device handler csr -> Cisco CSR
+    return m
 
-# yang_model = "toaster"
-# schema = n.get_schema(yang_model)
-# print(schema.data)
 
-# c = n.get_config(source='running').data_xml
-# with open("%s.xml" % host, 'w') as file:
-#     file.write(c)
+def get_capabilities(m):
+    for c in m.server_capabilities:  # display the NETCONF server capabilities
+        print(c)
 
-rpc = """
-    <get-config>
-    <source>
-        <running/>
-    </source>
-    <filter xmlns="urn:opendaylight:params:xml:ns:yang:hello">
-        <hello/>
-    </filter>
-    </get-config>"""
 
-data = n.rpc(rpc)
-print(data)
+def get_config(m):
+    # retrieve the running config from the NETCONF server using get-config and write the XML config to file
+    c = m.get_config(source='running').data_xml
+    with open("get_config.xml", 'w') as file:
+        file.write(c)
 
-n.close_session()
+
+def edit_config(m):
+    rpc = """
+        <input xmlns="urn:opendaylight:params:xml:ns:yang:hello">
+            <name>Laura</name>
+        </input>
+    """
+    print(m.rpc(rpc))
+
+
+def get_yang_schema(yang_model):  # obtain a YANG model specified by yang_model
+    schema = n.get_schema(yang_model)
+    print(schema)
+
+
+if __name__ == '__main__':
+    h = '10.1.7.81'
+    n = connect(h)
+    # get_capabilities(n)
+    # get_config(n)
+    # edit_config(n)
+    get_yang_schema('hello')
+
+    # n.close_session()
