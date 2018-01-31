429ed97ca4c152aac129a30a8e8878fb6ddf2095
lrodrin <lrodrin@gmail.com>
2018-01-31 11:29:25 +0100

    update startup

--- sdm_node_startup.py
+++ sdm_node_startup.py
@@ -4,33 +4,41 @@ This module create the node configuration
 Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 """
 
-import data as t
+import data as d
 
-if __name__ == '__main__':
-    host = '10.1.7.81'
-    port = 830
-    connection = t.connect(host, port, 'root', 'netlabN.')  # connection to NETCONF server
+# datastore sessions
+session_startup = 'startup'
+session_running = 'running'
+session_candidate = 'candidate'
 
-    filter = '''<sdm-node xmlns="urn:cttc:params:xml:ns:yang:sdm-node">'''
+# NETCONF operations
+operation_merge = 'merge'
+operation_replace = 'replace'
 
-    # datastore sessions
-    session1 = 'startup'
-    session2 = 'running'
-    session3 = 'candidate'
 
-    # operations
-    operation1 = 'merge'
-    operation2 = 'replace'
+def create_node_config(host, port, login, password, config_file, session, operation, namespace):
+    connection = d.connect(host, port, login, password)  # connection to NETCONF server
 
     try:
-        filename = open('sdm_node_config.xml')  # open configuration file
-        t.edit_config(connection, filename.read(), session2, operation1)  # create node configuration
-        filename.close()
+        f = open(config_file)  # open configuration file
+        d.edit_config(connection, f.read(), session, operation)  # create node configuration
+        f.close()
         print("new node configuration created\nnew configuration:")
-        t.get_config(connection, filter, session2)  # get node configuration
+        d.get_config(connection, namespace, session)  # get node configuration
 
     except Exception as e:
         print(e)
 
     finally:
         connection.close_session()
+
+
+if __name__ == '__main__':
+    host = '10.1.7.81'
+    port = 830
+    login = 'root'
+    password = 'netlabN.'
+    config_file = 'sdm_node_config.xml'
+    namespace = '''<sdm-node xmlns="urn:cttc:params:xml:ns:yang:sdm-node">'''
+
+    create_node_config(host, port, login, password, config_file, session_running, operation_merge, namespace)
