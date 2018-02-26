ca215c2d2307ef1960e65c72703614b90523ae22
lrodrin <lrodrin@gmail.com>
2018-02-26 16:58:07 +0100

    nou fitxer bash per a instal·lar el servidor des d'un click

--- install_netopeer.sh
+++ install_netopeer.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-!/bin/sh
+!/bin/bash
 
 apt-get install zlib1g-dev libssl-dev libavl-dev libev-dev coreutils acl valgrind autoconf libtool
 apt-get install gcc make cmake doxygen swig python-dev lua5.2 git build-essential devscripts debhelper
@@ -17,7 +16,7 @@ rm cmocka-1.0.1.tar.xz
 echo "Building libyang library from source"
 git clone https://github.com/CESNET/libyang.git
 cd libyang; mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
 make -j8 && make install
 cd ../..
 
@@ -33,7 +32,7 @@ rm libssh-0.7.5.tar.gz
 echo "Building libnetconf2 library from source"
 git clone https://github.com/CESNET/libnetconf2.git
 cd libnetconf2; mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
 make -j8 && make install
 cd ../..
 
@@ -46,7 +45,7 @@ cd ..
 echo "Building sysrepo library from source"
 git clone https://github.com/sysrepo/sysrepo.git
 cd sysrepo; mkdir build; cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
 make -j8 && make install
 cd ../..
 
