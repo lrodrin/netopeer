05407b0396581bcca8dc098d04bc685961c8298d
lrodrin <lrodrin@gmail.com>
2018-02-27 11:42:34 +0100

    update project

--- install_netopeer.sh
+++ install_netopeer.sh
@@ -1,17 +1,14 @@
-!/bin/bash
+#!/bin/bash
 
 apt-get install zlib1g-dev libssl-dev libavl-dev libev-dev coreutils acl valgrind autoconf libtool
 apt-get install gcc make cmake doxygen swig python-dev lua5.2 git build-essential devscripts debhelper
-apt-get install bison flex libpcre3-dev libprotobuf-c-dev protobuf-c-compiler 
+apt-get install bison flex libpcre3-dev libprotobuf-c-dev protobuf-c-compiler python-codecov
 
 echo "Building cmocka library from source"
-wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-tar -xJvf cmocka-1.0.1.tar.xz
-mv cmocka-1.0.1 cmocka
+git clone git://git.cryptomilk.org/projects/cmocka.git
 cd cmocka && mkdir build && cd build
 cmake .. && make -j8 && make install
 cd ../..
-rm cmocka-1.0.1.tar.xz
 
 echo "Building libyang library from source"
 git clone https://github.com/CESNET/libyang.git
@@ -21,16 +18,13 @@ make -j8 && make install
 cd ../..
 
 echo "Building libssh library from source"
-wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.7.5.tar.gz
-tar -xzf libssh-0.7.5.tar.gz
-mv libssh-0.7.5 libssh
+git clone -b v0-7 git://git.libssh.org/projects/libssh.git
 cd libssh; mkdir build; cd build
 cmake .. && make -j8 && make install
 cd ../..
-rm libssh-0.7.5.tar.gz
 
 echo "Building libnetconf2 library from source"
-git clone https://github.com/CESNET/libnetconf2.git
+git clone -b devel https://github.com/CESNET/libnetconf2.git
 cd libnetconf2; mkdir build; cd build
 cmake ..
 make -j8 && make install
@@ -39,7 +33,8 @@ cd ../..
 echo "Building protobuf library from source"
 git clone https://github.com/google/protobuf.git
 cd protobuf
-./autogen.sh && ./configure
+./autogen.sh
+./configure
 make -j8 && make install
 cd ..
 
@@ -50,20 +45,19 @@ cmake ..
 make -j8 && make install
 cd ../..
 
-pip install codecov
-
 echo "Building Netopeer2 from source"
 git clone https://github.com/CESNET/Netopeer2.git
 cd Netopeer2
 echo "Building keystored"
 cd keystored && mkdir build && cd build
+sysrepoctl --install --yang=/root/sysrepo/build/repository/yang/ietf-keystore@2016-10-31.yang --owner=root:root --permissions=666
 cmake ..
-make -j8 && make install 
+make && make install 
 cd ../..
 echo "Building server"
 cd server && mkdir build && cd build
 cmake ..
-make -j8 && make install && 
+make && make install
 cd ../..
 echo "Building client"
 cd cli && mkdir build && cd build
