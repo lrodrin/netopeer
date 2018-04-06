356d66bdbdbe687f990ff4b3e4646cc765303e12
lrodrin <lrodrin@gmail.com>
2018-04-06 12:40:06 +0200

    test1_edit_config.xml

--- kddi/generate_xml.py
+++ kddi/generate_xml.py
@@ -11,10 +11,7 @@ from kddi.data import pretty_print
 INDENT = ' ' * 4
 
 modes = ["LP01", "LP11a", "LP11b", "LP21a", "LP21b", "LP02"]
-cores = ["Core19", "Core18", "Core17", "Core16", "Core15",
-         "Core14", "Core13", "Core12", "Core11", "Core10",
-         "Core9", "Core8", "Core7", "Core6", "Core5",
-         "Core4", "Core3", "Core2", "Core1"]
+channels = ["137", "129", "121", "113", "105", "97", "89", "81", "73", "65", "57", "49", "41", "33", "25", "17"]
 
 
 def generate(filename, id_slice, conste, fs, bw):
@@ -24,27 +21,27 @@ def generate(filename, id_slice, conste, fs, bw):
     sliceid = etree.SubElement(slice, 'sliceid')
     sliceid.text = '%s' % id_slice
     counter = 1
-    for c in range(0, len(cores)):
-        for m in range(0, len(modes)):
+    for i in range(0, len(modes)):
+        for j in range(0, len(channels)):
             optical_channel = etree.SubElement(slice, 'optical-channel')
             # optical_channels parameters
             opticalchannelid = etree.SubElement(optical_channel, 'opticalchannelid')
             opticalchannelid.text = '%s' % counter
             coreid = etree.SubElement(optical_channel, 'coreid')
-            coreid.text = '%s' % cores[c]
+            coreid.text = 'Core1'
             modeid = etree.SubElement(optical_channel, 'modeid')
-            modeid.text = '%s' % modes[m]
+            modeid.text = '%s' % modes[i]
 
             frequency_slot = etree.SubElement(optical_channel, 'frequency-slot')
             # frequency_slot parameters
             ncf = etree.SubElement(frequency_slot, 'ncf')
-            ncf.text = '41'
+            ncf.text = '%s' % channels[j]
             slot_width = etree.SubElement(frequency_slot, 'slot-width')
             slot_width.text = '%s' % fs
             frequency_slot.append(slot_width)
             counter += 1
 
-    for i in range(1, len(cores) * len(modes) + 1):
+    for i in range(1, len(channels) * len(modes) + 1):
         optical_signal = etree.SubElement(slice, 'optical-signal')
         # optical_signal parameters
         opticalchannelid = etree.SubElement(optical_signal, 'opticalchannelid')
@@ -63,18 +60,18 @@ def generate(filename, id_slice, conste, fs, bw):
         mimo = etree.SubElement(equalization, 'mimo')
         mimo.text = 'true'
         num_taps = etree.SubElement(equalization, 'num_taps')
-        num_taps.text = '200'
+        num_taps.text = '500'
 
-        monitor = etree.SubElement(optical_signal, 'monitor')
+        # monitor = etree.SubElement(optical_signal, 'monitor')
         # monitor parameters
-        ber = etree.SubElement(monitor, 'ber')
-        ber.text = 'ber'
+        # ber = etree.SubElement(monitor, 'ber')
+        # ber.text = 'ber'
         # channel_power = etree.SubElement(monitor, 'channel-power')
         # channel_power.text = 'channel-power'
         # noise_level = etree.SubElement(monitor, 'noise-level')
         # noise_level.text = 'noise-level'
-        osnr = etree.SubElement(monitor, 'osnr')
-        osnr.text = 'osnr'
+        # osnr = etree.SubElement(monitor, 'osnr')
+        # osnr.text = 'osnr'
 
     xml = etree.tostring(config)
     pretty_xml = pretty_print(xml)
@@ -83,4 +80,4 @@ def generate(filename, id_slice, conste, fs, bw):
 
 
 if __name__ == '__main__':
-    generate("test1_edit_config.xml", 1, 'qam16', 1, '12000000000')
+    generate("test1_edit_config.xml", 1, 'qam16', 2, '12000000000')
