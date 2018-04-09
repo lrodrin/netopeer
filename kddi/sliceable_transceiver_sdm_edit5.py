c0599faa5e4fee86f5be1ed92cd3a8318e38f098
lrodrin <lrodrin@gmail.com>
2018-04-09 15:30:30 +0200

    update sliceable_transceiver_sdm_edit5.py

--- kddi/sliceable_transceiver_sdm_edit5.py
+++ kddi/sliceable_transceiver_sdm_edit5.py
@@ -7,25 +7,19 @@ Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 import data as d
 
 
-def set_parameters(host, port, login, password, config_file, session, operation, filter):
-    connection = d.connect(host, port, login, password)
-
+def set_parameters(connection, config_file, session, operation, filter):
     try:
         f = open(config_file)
         d.edit_config(connection, f.read(), session, operation)
         f.close()
-        print("new sliceable-transceiver-sdm configuration created\nnew configuration:")
         print(d.get_config(connection, filter, session))
 
     except Exception as e:
         print(e)
 
-    finally:
-        connection.close_session()
-
 
 if __name__ == '__main__':
-    host = '10.1.7.83'
+    host = '10.1.7.67'
     host2 = '10.1.7.83'
     port = 830
     login = 'root'
@@ -33,5 +27,11 @@ if __name__ == '__main__':
     config_file = 'test5_edit_config.xml'
     filter = "<transceiver/>"
 
-    set_parameters(host, port, login, password, config_file, 'running', 'merge', filter)
-    set_parameters(host2, port, login, password, config_file, 'running', 'merge', filter)
+    connectionTX = d.connect(host, port, login, password)
+    connectionRX = d.connect(host2, port, login, password)
+
+    set_parameters(connectionTX, config_file, 'running', 'merge', filter)
+    set_parameters(connectionRX, config_file, 'running', 'merge', filter)
+
+    connectionTX.close_session()
+    connectionRX.close_session()
--- kddi/tests.py
+++ kddi/tests.py
@@ -4,13 +4,44 @@ This module implements execution tests
 Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 """
 
-import subprocess
 import time
 
-subprocess.call("python sliceable_transceiver_sdm_startup.py", shell=True)  # test1
+import sliceable_transceiver_sdm_edit5 as edit5
+import sliceable_transceiver_sdm_get as get
+import sliceable_transceiver_sdm_startup as startup
+
+host = '10.0.34.108'
+host2 = '10.0.34.112'
+port = 830
+login = 'root'
+password = 'root'
+filter = "<transceiver/>"
+
+# TEST 1
+config_file = 'test1_edit_config.xml'
+
+connectionTX = startup.init_connection(host, port, login, password)
+connectionRX = startup.init_connection(host2, port, login, password)
+startup.create_configuration(connectionTX, config_file, 'running', 'merge', filter)
+startup.create_configuration(connectionRX, config_file, 'running', 'merge', filter)
+
+# TEST 2
+get.get_ber_and_osnr_parameters(connectionRX)
+
+# Sleeping
 time.sleep(30)
-subprocess.call("python sliceable_transceiver_sdm_get.py", shell=True)  # test2
-time.sleep(30)
-subprocess.call("python sliceable_transceiver_sdm_get.py", shell=True)  # test4
-subprocess.call("python sliceable_transceiver_sdm_edit5.py", shell=True)  # test5
-subprocess.call("python sliceable_transceiver_sdm_get.py", shell=True)  # test6
+
+# TEST 4
+get.get_ber_and_osnr_parameters(connectionRX)
+
+# TEST 5
+config_file = 'test5_edit_config.xml'
+edit5.set_parameters(connectionTX, config_file, 'running', 'merge', filter)
+edit5.set_parameters(connectionRX, config_file, 'running', 'merge', filter)
+
+# TEST 6
+get.get_ber_and_osnr_parameters(connectionRX)
+
+# END
+startup.close_connection(connectionTX)
+startup.close_connection(connectionRX)
