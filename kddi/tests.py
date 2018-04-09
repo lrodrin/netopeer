4cbebc334795624a67f206194ef827d53d4cd5b5
lrodrin <lrodrin@gmail.com>
2018-04-09 16:41:04 +0200

    update sliceable_transceiver_sdm_edit.py

--- kddi/tests.py
+++ kddi/tests.py
@@ -6,40 +6,46 @@ Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 
 import time
 
-import sliceable_transceiver_sdm_edit5 as edit5
-import sliceable_transceiver_sdm_get as get
-import sliceable_transceiver_sdm_startup as startup
+import kddi.sliceable_transceiver_sdm_edit as edit
+import kddi.sliceable_transceiver_sdm_get as get
+import kddi.sliceable_transceiver_sdm_startup as startup
 
-host = '10.0.34.108'
-host2 = '10.0.34.112'
+host = '10.1.7.67'
+host2 = '10.1.7.83'
 port = 830
 login = 'root'
-password = 'root'
+password = 'netlabN.'
 filter = "<transceiver/>"
+
+# OPEN CONNECTION
+connectionTX = startup.init_connection(host, port, login, password)
+connectionRX = startup.init_connection(host2, port, login, password)
 
 # TEST 1
 config_file = 'test1_edit_config.xml'
-
-connectionTX = startup.init_connection(host, port, login, password)
-connectionRX = startup.init_connection(host2, port, login, password)
+print("TEST 1\nConfiguration of one spectral and spatial superchannel")
 startup.create_configuration(connectionTX, config_file, 'running', 'merge', filter)
 startup.create_configuration(connectionRX, config_file, 'running', 'merge', filter)
 
 # TEST 2
+print("TEST 2\nMeasurement of the OSNR and BER")
 get.get_ber_and_osnr_parameters(connectionRX)
 
 # Sleeping
 time.sleep(30)
 
 # TEST 4
+print("TEST 4\nMeasurement of the OSNR and BER after the soft failure in the reference core")
 get.get_ber_and_osnr_parameters(connectionRX)
 
 # TEST 5
+print("TEST 5\nReconfiguration of the optical channels")
 config_file = 'test5_edit_config.xml'
-edit5.set_parameters(connectionTX, config_file, 'running', 'merge', filter)
-edit5.set_parameters(connectionRX, config_file, 'running', 'merge', filter)
+edit.set_parameters(connectionTX, config_file, 'running', 'merge', filter)
+edit.set_parameters(connectionRX, config_file, 'running', 'merge', filter)
 
 # TEST 6
+print("TEST 6\nMeasurement of the OSNR and BER to ensure optical restoration")
 get.get_ber_and_osnr_parameters(connectionRX)
 
 # END
