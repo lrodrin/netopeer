6111c3e827082c34c9277f894e808a82381dc012
lrodrin <lrodrin@gmail.com>
2018-04-09 15:19:52 +0200

    update sliceable_transceiver_sdm_startup.py

--- kddi/sliceable_transceiver_sdm_startup.py
+++ kddi/sliceable_transceiver_sdm_startup.py
@@ -7,9 +7,12 @@ Copyright (c) 2018-2019 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 import data as d
 
 
-def create_configuration(host, port, login, password, config_file, session, operation, filter):
+def init_connection(host, port, login, password):
     connection = d.connect(host, port, login, password)
+    return connection
 
+
+def create_configuration(connection, config_file, session, operation, filter):
     try:
         f = open(config_file)
         d.edit_config(connection, f.read(), session, operation)
@@ -20,12 +23,13 @@ def create_configuration(host, port, login, password, config_file, session, oper
     except Exception as e:
         print(e)
 
-    finally:
-        connection.close_session()
+
+def close_connection(connection):
+    connection.close_session()
 
 
 if __name__ == '__main__':
-    host = '10.1.7.83'
+    host = '10.1.7.67'
     host2 = '10.1.7.83'
     port = 830
     login = 'root'
@@ -33,5 +37,10 @@ if __name__ == '__main__':
     config_file = 'test1_edit_config.xml'
     filter = "<transceiver/>"
 
-    create_configuration(host, port, login, password, config_file, 'running', 'merge', filter)
-    create_configuration(host2, port, login, password, config_file, 'running', 'merge', filter)
+    connectionTX = init_connection(host, port, login, password)
+    create_configuration(connectionTX, config_file, 'running', 'merge', filter)
+    close_connection(connectionTX)
+
+    connectionRX = init_connection(host2, port, login, password)
+    create_configuration(connectionRX, config_file, 'running', 'merge', filter)
+    close_connection(connectionRX)
