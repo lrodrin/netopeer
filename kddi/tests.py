d84374301543334fa8b3585887ee927885406e05
lrodrin <lrodrin@gmail.com>
2018-04-09 13:11:48 +0200

    update project

--- kddi/tests.py
+++ kddi/tests.py
@@ -5,9 +5,12 @@ Copyright (c) 2017-2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
 """
 
 import subprocess
+import time
 
 subprocess.call("python sliceable_transceiver_sdm_startup.py", shell=True)  # test1
+time.sleep(30)
 subprocess.call("python sliceable_transceiver_sdm_get.py", shell=True)  # test2
+time.sleep(30)
 subprocess.call("python sliceable_transceiver_sdm_get.py", shell=True)  # test4
 subprocess.call("python sliceable_transceiver_sdm_edit5.py", shell=True)  # test5
 subprocess.call("python sliceable_transceiver_sdm_get.py", shell=True)  # test6
