16a4c05cca3c9aa064bbdec6aa6181920abac143
lrodrin <lrodrin@gmail.com>
2018-05-03 15:43:46 +0200

    refactor netconf_api

--- plugin/topology/netconf_api.py
+++ plugin/topology/netconf_api.py
@@ -1,4 +1,7 @@
 import logging
+import os
+import sys
+import traceback
 
 from ncclient import manager
 
@@ -6,87 +9,37 @@ __author__ = "Laura Rodriguez <laura.rodriguez@cttc.cat>"
 __copyright__ = "Copyright 2018, CTTC"
 __license__ = "MIT License"
 
-# logger = logging.getLogger('.'.join(os.path.abspath(__name__).split('/')[1:]))
-logging.basicConfig(filename='netconf_plugin.log', filemode='w', level=logging.DEBUG)
+logger = logging.getLogger('.'.join(os.path.abspath(__name__).split('/')[1:]))
 
 
 class NetopeerAPIaccessor:
-    ip_agent_CS = '10.1.7.65'
-    ip_agent_CO = '10.1.7.66'
-    port = '830'
-    user = 'root'
-    password = 'netlabN.'
+
+    def __init__(self, user, password, ip, port, ):
+
+        self.user = user
+        self.password = password
+        self.ip = ip
+        self.port = port
 
     def retrieveConfiguration(self):
-        # TODO: connection and get methods
-        # connection_CS = manager.connect(host=self.ip_agent_CS, port=self.port, username=self.user,
-        #                                 password=self.password,
-        #                                 hostkey_verify=False, device_params={'name': 'default'}, allow_agent=False,
-        #                                 look_for_keys=False)
-        connection_CO = manager.connect(host=self.ip_agent_CO, port=self.port, username=self.user,
-                                        password=self.password,
-                                        hostkey_verify=False, device_params={'name': 'default'}, allow_agent=False,
-                                        look_for_keys=False)
+        logger.debug('Retrieving config from {}:{}'.format(self.ip, self.port))
+        try:
+            connection = manager.connect(host=self.ip, port=self.port, username=self.user,
+                                         password=self.password, hostkey_verify=False,
+                                         device_params={'name': 'default'}, allow_agent=False,
+                                         look_for_keys=False)
+            logger.debug('Response from {}:\n\t{}'.format(connection, connection.content))
 
-        # logger.debug('Response connection_CS to NETCONF server ' + str(connection_CS))
-        logging.debug('Response connection_CO to NETCONF server ' + str(connection_CO))
+            configuration = connection.get_config(source='running', filter=('subtree', '<transceiver/>')).data_xml
+            logger.debug('Response from {}:\n\t{}'.format(connection, configuration))
 
-        # configuration_CO = connection_CS.get_config(source='running', filter=('subtree', '<transceiver/>')).data_xml
-        configuration_CO = connection_CO.get_config(source='running', filter=('subtree', '<transceiver/>')).data_xml
+        except Exception as e:
+            logger.error({'error': str(sys.exc_info()[0]), 'value': str(sys.exc_info()[1]),
+                          'traceback': str(traceback.format_exc()), 'code': 500})
 
-        # logger.debug('Response get configuration_CO ' + str(configuration_CS))
-        logging.debug('Response get configuration_CO ' + str(configuration_CO))
-        #
-        # return configuration_CS, configuration_CO
-        return configuration_CO
+            raise e
+        return configuration
 
-
-# TEST
-# api = NetopeerAPIaccessor()
-# print(api.retrieveConfiguration())
-
-# def retrieveNodes(self):
-#     http_json = 'http://' + self.ip + ':' + str(self.port) + '/restconf/operational/opendaylight-inventory:nodes'
-#
-#     response = requests.get(http_json, auth=HTTPBasicAuth(self.user, self.password))
-#     nodes = response.json()
-#     return nodes
-
-# def getPortListFromNode(self, nodeId, nodes=None):
-#     if not nodes:
-#         nodes = self.retrieveNodes()
-#     for node in nodes.get('nodes')['node']:
-#         for nc in node['node-connector']:
-#             if self.get_datapath_id(int(nc['id'].split(':')[1])) == nodeId:
-#                 return node['node-connector']
-#     return None
-
-# def getPortDetails(self, nodeId, portId):
-#     http_json = 'http://' + self.ip + ':' + str(
-#         self.port) + '/restconf/operational/opendaylight-inventory:nodes/node/' + str(
-#         nodeId) + '/node-connector/' + str(portId)
-#     logger.debug(str(http_json))
-#     response = requests.get(http_json, auth=HTTPBasicAuth(self.user, self.password))
-#     port = response.json()
-#     logger.debug('Response %s', str(port))
-#     return port
-
-# def getdpid(self, hw_address):
-#     return '00:00:' + str(hw_address).lower()
-
-# def get_datapath_id(self, routerID):
-#     routerHex = hex(routerID)
-#     routerHex = routerHex[2:]  # remove 0x
-#     if routerHex[-1:] == 'L':
-#         routerHex = routerHex[:-1]
-#     prefix_num = 16 - len(routerHex)
-#     logger.debug('Router Id raw' + str(routerHex))
-#     for i in range(0, prefix_num):
-#         routerHex = '0' + str(routerHex)
-#
-#     for i in range(0, len(routerHex) / 2 - 1):
-#         routerHex = routerHex[:(i + 1) * 2 + i] + ':' \
-#                     + routerHex[(i + 1) * 2 + i:]
-#
-#     logger.debug('getDatapathID. routerID: %s , routerHex: %s', routerID, routerHex)
-#     return routerHex
+# if __name__ == '__main__':
+#     api = NetopeerAPIaccessor('root', 'netlabN.', '10.1.7.66', 830)
+#     print(api.retrieveConfiguration())
