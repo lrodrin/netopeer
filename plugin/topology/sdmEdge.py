ed1b875d602937304398b4277d4c2a59b7b0b710
lrodrin <lrodrin@gmail.com>
2018-07-20 08:22:27 +0200

    delete plugin because is in another private project

--- plugin/topology/sdmEdge.py
+++
@@ -1,8 +0,0 @@
-from lib.COP.objects_service_topology.edge import Edge
-
-
-class SdmEdge(Edge):
-
-    def __init__(self, json_struct=None):
-        self.delay = ""  # TODO: add to schema(?)
-        super(SdmEdge, self).__init__(json_struct)
