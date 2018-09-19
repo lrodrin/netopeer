63e474cd73fb6392c9149df6352c86fe8ad03667
lrodrin <lrodrin@gmail.com>
2018-09-19 16:24:17 +0200

    new implementation for ofc

---
+++ ofc/generate_fs2_xml.py
@@ -0,0 +1,73 @@
+"""
+This module generate XML configuration for step 3 and 4
+
+Copyright (c) 2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
+"""
+import xml.dom.minidom as md
+
+from lxml import etree
+
+INDENT = ' ' * 4
+
+modes = ["LP01", "LP11a", "LP11b", "LP21a", "LP21b", "LP02"]
+channels = ["39", "41"]
+
+
+def pretty_print(s):
+    return '\n'.join(line for line in md.parseString(s).toprettyxml(indent=INDENT).split('\n') if line.strip())
+
+
+def generate(filename, bw, qam, mm, taps, num_modes):
+    config = etree.Element('config', xmlns="urn:ietf:params:xml:ns:netconf:base:1.0")
+    transceiver = etree.SubElement(config, 'transceiver-connectivity',
+                                   xmlns="urn:sliceable-transceiver-sdm-connectivity")
+    slice = etree.SubElement(transceiver, 'slice')
+    sliceid = etree.SubElement(slice, 'sliceid')
+    sliceid.text = '1'
+    counter = 1
+    for j in range(0, len(channels)):
+        for i in range(1, num_modes + 1):
+            optical_channel = etree.SubElement(slice, 'optical-channel')
+            opticalchannelid = etree.SubElement(optical_channel, 'opticalchannelid')
+            opticalchannelid.text = '%s' % counter
+            coreid = etree.SubElement(optical_channel, 'coreid')
+            coreid.text = 'Core19'
+            modeid = etree.SubElement(optical_channel, 'modeid')
+            modeid.text = '%s' % modes[i - 1]
+
+            frequency_slot = etree.SubElement(optical_channel, 'frequency-slot')
+            ncf = etree.SubElement(frequency_slot, 'ncf')
+            ncf.text = '%s' % channels[j]
+            slot_width = etree.SubElement(frequency_slot, 'slot-width')
+            slot_width.text = '1'
+            frequency_slot.append(slot_width)
+            counter += 1
+
+    for i in range(1, len(channels) * num_modes + 1):
+        optical_signal = etree.SubElement(slice, 'optical-signal')
+        opticalchannelid = etree.SubElement(optical_signal, 'opticalchannelid')
+        opticalchannelid.text = '%s' % i
+        constellation = etree.SubElement(optical_signal, 'constellation')
+        constellation.text = '%s' % qam
+        bandwidth = etree.SubElement(optical_signal, 'bandwidth')
+        bandwidth.text = '%s' % bw
+        fec = etree.SubElement(optical_signal, 'fec')
+        fec.text = 'sd-fec'
+
+        equalization = etree.SubElement(optical_signal, 'equalization')
+        equalizationid = etree.SubElement(equalization, 'equalizationid')
+        equalizationid.text = '%s' % i
+        mimo = etree.SubElement(equalization, 'mimo')
+        mimo.text = '%s' % mm
+        num_taps = etree.SubElement(equalization, 'num_taps')
+        num_taps.text = '%s' % taps
+
+    xml = etree.tostring(config)
+    pretty_xml = pretty_print(xml)
+    with open(filename, "w") as f:
+        f.write(pretty_xml)
+
+
+if __name__ == '__main__':
+    generate("edit_3.xml", 12000000000, "qam16", "LMS 12x12", 500, 6)
+    generate("edit_4.xml", 12000000000, "qam64", "LMS 6x6", 200, 3)
---
+++ ofc/steps.py
@@ -0,0 +1,88 @@
+"""
+This module implements the steps
+
+Copyright (c) 2018 Laura Rodriguez Navas <laura.rodriguez.navas@cttc.cat>
+"""
+from ncclient import manager
+
+
+def connect(host):
+    try:
+        connection = manager.connect(host=host, port=830, username='root', password='netlabN.', hostkey_verify=False,
+                                     device_params={'name': 'default'}, allow_agent=False, look_for_keys=False)
+        return connection
+
+    except Exception as e:
+        print(e)
+
+
+def edit(connection, config_file, session, operation):
+    try:
+        f = open(config_file)
+        connection.edit_config(target=session, config=f.read(), default_operation=operation)
+        f.close()
+
+    except Exception as e:
+        print(e)
+
+
+def close(connection):
+    connection.close_session()
+
+
+def delete_4(connection, session):
+    try:
+        template = """<config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+            <transceiver-connectivity xmlns="urn:sliceable-transceiver-sdm-connectivity">
+                <slice>
+                    <sliceid>1</sliceid>
+                        <optical-channel><opticalchannelid>4</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>5</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>6</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>10</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>11</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>12</opticalchannelid></optical-channel>
+                </slice>
+            </transceiver-connectivity>
+        </config>"""
+        connection.edit_config(target=session, config=template, default_operation='replace')
+
+    except Exception as e:
+        print(e)
+
+
+def delete_5(connection, session):
+    try:
+        template = """<config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+            <transceiver-connectivity xmlns="urn:sliceable-transceiver-sdm-connectivity">
+                <slice>
+                    <sliceid>1</sliceid>
+                        <optical-channel><opticalchannelid>1</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>2</opticalchannelid></optical-channel>
+                        <optical-channel><opticalchannelid>3</opticalchannelid></optical-channel>
+                </slice>
+            </transceiver-connectivity>
+        </config>"""
+        connection.edit_config(target=session, config=template, default_operation='replace')
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    hostRX = '10.1.7.66'
+    config_files = ["edit_1.xml", "edit_2.xml", "edit_3.xml", "edit_4.xml", "edit_5.xml"]
+    connectionRX = connect(hostRX)
+    # STEP 1
+    edit(connectionRX, config_files[0], 'running', 'merge')
+    # STEP 2
+    edit(connectionRX, config_files[1], 'running', 'merge')
+    # STEP 3
+    edit(connectionRX, config_files[2], 'running', 'merge')
+    # STEP 4
+    delete_4(connectionRX, 'running')
+    edit(connectionRX, config_files[3], 'running', 'replace')
+    # STEP 5
+    delete_5(connectionRX, 'running')
+    edit(connectionRX, config_files[4], 'running', 'replace')
+    close(connectionRX)
